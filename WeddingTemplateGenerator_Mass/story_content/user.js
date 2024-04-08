10b387c3b648fe6a23ea3ebbceee610455d9c182
Max Ho <maxhowl86@gmail.com>
2024-04-08 22:08:04 +0800

    Migrated from vendor

---
+++ WeddingTemplateGenerator_Mass/story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5uiIP2mn2Zf":
+        Script1();
+        break;
+      case "60rWsPEBQt6":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.open("templateMass_C.html")
+}
+
+function Script2()
+{
+  window.open("templateMass_D.html")
+}
+
---
+++ WeddingTemplateGenerator_Service/story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6doPkeRHJTx":
+        Script1();
+        break;
+      case "5lULObiKKCO":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.open("templateService_D.html")
+}
+
+function Script2()
+{
+  window.open("templateService_C.html")
+}
+
